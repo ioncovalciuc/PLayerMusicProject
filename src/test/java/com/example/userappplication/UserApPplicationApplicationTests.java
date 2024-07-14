85e873022001ffc62f24c47264441843263e5b2f
Ion Covalciuc <covalciuc00@gmail.com>
2024-07-14 14:06:47 +0300

    I created a music player using XML file configuration

---
+++ src/test/java/com/example/userappplication/UserApPplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userappplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApPplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
