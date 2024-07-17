aafd1a113c33551f8a9518c04e8df7a57f118c94
Ion Covalciuc <covalciuc00@gmail.com>
2024-07-17 23:11:40 +0300

    new DI

---
+++ src/main/java/com/example/userappplication/Computer.java
@@ -0,0 +1,24 @@
+package com.example.userappplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Computer {
+    private int ID;
+    private MusicPlayer musicPlayer;
+    @Autowired
+    public Computer(MusicPlayer musicPlayer) {
+        this.ID = 1;
+        this.musicPlayer = musicPlayer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "ID=" + ID +
+                ", musicPlayer=" + musicPlayer.playMusic() +
+                '}';
+    }
+}
